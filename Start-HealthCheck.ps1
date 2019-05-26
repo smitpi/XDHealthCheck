9ae2dee5cbbadb7abd00b1cfb5467ecd57a743c8
Pierre Smit <apps@htpcza.com>
2019-05-26 09:47:24 +0200

    11

--- Start-HealthCheck.ps1
+++ Start-HealthCheck.ps1
@@ -41,10 +41,10 @@ Date Created - 24/05/2019_19:23
 
 Param()
 
-
+#region 
 [string]$ScriptPath = $PSScriptRoot
 [xml]$Parameters = Get-Content .\Parameters.xml # Read content of XML file
-
-
 import-module CTXHealthCheck -Force -Verbose
 Initialize-CitrixHealthCheck -XMLParameter $Parameters -ScriptPath $ScriptPath -Verbose
+#endregion
+
