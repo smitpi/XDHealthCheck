0e4b4b8cbca2ec065064d181dd19db0d82020402
Pierre Smit <smitpi@users.noreply.github.com>
2021-03-06 20:50:34 +0200

    1

--- Private/Start-LabGui.ps1
+++ Private/Start-LabGui.ps1
@@ -20,3 +20,19 @@ $params = @{
 	Verbose               = $false
 }
 Show-ScriptMenuGui @params
+}
+
+
+Join-Path -Path ((Get-Module XDHealthCheck).ModuleBase).ToString() -ChildPath 'Public\Reporting\Start-XDMenu.ps1'
+
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Bypass -WindowStyle Hidden -File "C:\full\path\PSScriptMenuGui_example\PSScriptMenuGui.ps1"
+	
+$detail = "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Bypass -WindowStyle Hidden -File " + (Join-Path -Path ((Get-Module XDHealthCheck).ModuleBase).ToString() -ChildPath "Public\Reporting\Start-XDMenu.ps1") + '"'
+
+
+New-Item -Path C:\Users\Public\Desktop -Name 'Run XDHealthCheck.bat' -ItemType File -Value 'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Bypass -WindowStyle Hidden -Command "& {Import-Module XDHealthCheck -force;Start-XDMenu}"'
+
+
+Import-Module XDHealthCheck -force;Start-XDMenu
+
+ -Command "& {Get-EventLog -LogName security}"
