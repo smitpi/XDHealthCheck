33a4bcf8ef6f2e76ff37d6b2116c33e2aeeb1df6
Pierre Smit <pierre.smit@outlook.com>
2022-05-04 00:06:28 +0200

    3

---
+++ XDHealthCheck/.history/Public/FarmDetails/Get-CitrixConfigurationChange_20220504000543.ps1
@@ -0,0 +1,112 @@
+
+<#PSScriptInfo
+
+.VERSION 1.0.13
+
+.GUID 71b2bc51-85ce-407b-ace5-96df009782d3
+
+.AUTHOR Pierre Smit
+
+.COMPANYNAME HTPCZA Tech
+
+.COPYRIGHT
+
+.TAGS Citrix
+ 
+.LICENSEURI
+
+.PROJECTURI
+
+.ICONURI
+
+.EXTERNALMODULEDEPENDENCIES
+
+.REQUIREDSCRIPTS
+
+.EXTERNALSCRIPTDEPENDENCIES 
+
+.RELEASENOTES
+Created [05/05/2019_09:00]
+Updated [13/05/2019_04:40]
+Updated [22/05/2019_20:13]
+Updated [24/05/2019_19:24]
+Updated [06/06/2019_19:25]
+Updated [09/06/2019_09:18]
+Updated [15/06/2019_01:11]
+Updated [15/06/2019_13:59] Updated Reports
+Updated [01/07/2020_14:43] Script File Info was updated
+Updated [01/07/2020_15:42] Script File Info was updated
+Updated [01/07/2020_16:07] Script File Info was updated
+Updated [01/07/2020_16:13] Script File Info was updated
+Updated [06/03/2021_20:58] Script File Info was updated
+Updated [15/03/2021_23:28] Script File Info was updated
+
+#> 
+
+<#
+
+.DESCRIPTION 
+Function for Citrix XenDesktop HTML Health Check Report
+
+<#
+.SYNOPSIS
+Show the changes that was made to the farm
+
+.DESCRIPTION
+Show the changes that was made to the farm
+
+.PARAMETER AdminServer
+Name of data collector
+
+.PARAMETER Indays
+Limit the search, to only show changes from the last couple of days
+
+.PARAMETER RemoteCredentials
+Enable function to run remotely, if the CItrix cmdlets are not available
+
+.EXAMPLE
+Get-CitrixConfigurationChange -DDC $CTXDDC -Indays 7 -RemoteCredentials $CTXAdmin
+
+#>
+Function Get-CitrixConfigurationChange {
+	[CmdletBinding()]
+	PARAM(
+		[Parameter(Mandatory = $true)]
+		[ValidateNotNull()]
+		[ValidateNotNullOrEmpty()]
+		[string]$AdminServer,
+		[Parameter(Mandatory = $true)]
+		[ValidateNotNull()]
+		[ValidateNotNullOrEmpty()]
+		[int32]$Indays
+	)
+
+	if (-not(Get-PSSnapin -Registered | Where-Object {$_.name -like 'Citrix*'})) {Add-PSSnapin citrix* -ErrorAction SilentlyContinue}
+
+	if (Test-Path $exportpath) { Remove-Item $exportpath -Force -ErrorAction SilentlyContinue }
+	Write-Verbose "$((Get-Date -Format HH:mm:ss).ToString()) [Progress] Exporting Changes"
+
+	Export-LogReportCsv -AdminAddress $AdminServer -OutputFile $exportpath -StartDateRange $startdate -EndDateRange (Get-Date)
+	Write-Verbose "$((Get-Date -Format HH:mm:ss).ToString()) [Progress] Importing Changes"
+
+	$LogExportAll = Import-Csv -Path $exportpath -Delimiter ','
+	$LogExport = $LogExportAll | Where-Object { $_.'High Level Operation Text' -notlike '' } | Select-Object -Property High*
+	$LogSum = $LogExportAll | Group-Object -Property 'High Level Operation Text' -NoElement
+
+	Remove-Item $exportpath -Force -ErrorAction SilentlyContinue
+	$CTXObject = New-Object PSObject -Property @{
+		DateCollected = (Get-Date -Format dd-MM-yyyy_HH:mm).ToString()
+		AllDetails    = $LogExportAll
+		Filtered      = $LogExport
+		Summary       = $LogSum
+	}
+	Write-Verbose "$((Get-Date -Format HH:mm:ss).ToString()) [Ending] Config Changes Details"
+
+	$CTXObject
+
+} #end Function
+
+		$CTXObject
+
+} #end Function
+
---
+++ XDHealthCheck/.history/Public/FarmDetails/Get-CitrixLicenseInformation_20220504000114.ps1
@@ -0,0 +1,107 @@
+
+<#PSScriptInfo
+
+.VERSION 1.0.13
+
+.GUID 7a62533c-d105-4718-9440-00957643908f
+
+.AUTHOR Pierre Smit
+
+.COMPANYNAME HTPCZA Tech
+
+.COPYRIGHT 
+
+.TAGS Citrix
+
+.LICENSEURI 
+
+.PROJECTURI 
+
+.ICONURI 
+
+.EXTERNALMODULEDEPENDENCIES 
+
+.REQUIREDSCRIPTS 
+
+.EXTERNALSCRIPTDEPENDENCIES 
+
+.RELEASENOTES
+Created [05/05/2019_09:00]
+Updated [13/05/2019_04:40]
+Updated [22/05/2019_20:13]
+Updated [24/05/2019_19:24]
+Updated [06/06/2019_19:25]
+Updated [09/06/2019_09:18]
+Updated [15/06/2019_01:11]
+Updated [15/06/2019_13:59] Updated Reports
+Updated [01/07/2020_14:43] Script Fle Info was updated
+Updated [01/07/2020_15:42] Script Fle Info was updated
+Updated [01/07/2020_16:07] Script Fle Info was updated
+Updated [01/07/2020_16:13] Script Fle Info was updated
+Updated [06/03/2021_20:58] Script Fle Info was updated
+Updated [15/03/2021_23:28] Script Fle Info was updated
+
+#> 
+
+
+
+<#
+
+.DESCRIPTION 
+Function for Citrix XenDesktop HTML Health Check Report
+
+#>
+
+
+
+<#
+.SYNOPSIS
+Show Citrix License details
+
+.DESCRIPTION
+Show Citrix License details
+
+.PARAMETER AdminServer
+Name of a data collector
+
+.PARAMETER RunAsPSRemote
+Credentials if running psremote 
+
+.PARAMETER RemoteCredentials
+Enable function to run remotely, if the CItrix cmdlets are not available
+
+.EXAMPLE
+Get-CitrixLicenseInformation -AdminServer $CTXDDC -RemoteCredentials $CTXAdmin -RunAsPSRemote
+
+#>Function Get-CitrixLicenseInformation {
+	[CmdletBinding()]
+	PARAM(
+		[Parameter(Mandatory = $true, Position = 0, ValueFromPipeline = $true)]
+		[ValidateNotNull()]
+		[ValidateNotNullOrEmpty()]
+		[string]$AdminServer)
+
+<<<<<<< HEAD
+       if (-not(Get-PSSnapin -Registered | Where-Object {$_.name -like "Citrix*"})) {Add-PSSnapin citrix* -ErrorAction SilentlyContinue}
+=======
+        Add-PSSnapin Citrix*
+>>>>>>> 633a3f34044f8c62952f31d9bd3aebdad87d4b25
+		Write-Verbose "$((Get-Date -Format HH:mm:ss).ToString()) [Starting] License Details"
+
+		$LicenseServer = Get-BrokerSite -AdminAddress $AdminServer | Select-Object LicenseServerName
+		[string]$licurl = "https://" + $LicenseServer.LicenseServerName + ":8083"
+		$cert = Get-LicCertificate -AdminAddress $licurl
+		$ctxlic = Get-LicInventory -AdminAddress $licurl -CertHash $cert.CertHash | Where-Object { $_.LicensesInUse -ne 0 }
+		[System.Collections.ArrayList]$LicDetails = @()
+		foreach ($lic in $ctxlic) {
+            $LicDetails.Add([pscustomobject]@{
+				LicenseProductName = $lic.LocalizedLicenseProductName
+				LicenseModel       = $lic.LocalizedLicenseModel
+				LicensesInstalled  = $lic.LicensesAvailable
+				LicensesInUse      = $lic.LicensesInUse
+				LicensesAvailable  = ([int]$lic.LicensesAvailable - [int]$lic.LicensesInUse)
+			})
+        }
+        $LicDetails
+} #end Function
+
