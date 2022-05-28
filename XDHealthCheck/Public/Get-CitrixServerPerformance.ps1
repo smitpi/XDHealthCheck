2e809efb2e80a2e6cab7bc09365b82ca940ee09a
Pierre Smit <pierre.smit@outlook.com>
2022-05-28 08:45:40 +0200

    Update Get-CitrixServerPerformance.ps1

--- XDHealthCheck/Public/Get-CitrixServerPerformance.ps1
+++ XDHealthCheck/Public/Get-CitrixServerPerformance.ps1
@@ -9,39 +9,26 @@
 
 .COMPANYNAME HTPCZA Tech
 
-.COPYRIGHT 
+.COPYRIGHT
 
 .TAGS Citrix
 
-.LICENSEURI 
+.LICENSEURI
 
-.PROJECTURI 
+.PROJECTURI
 
-.ICONURI 
+.ICONURI
 
-.EXTERNALMODULEDEPENDENCIES 
+.EXTERNALMODULEDEPENDENCIES
 
-.REQUIREDSCRIPTS 
+.REQUIREDSCRIPTS
 
-.EXTERNALSCRIPTDEPENDENCIES 
+.EXTERNALSCRIPTDEPENDENCIES
 
 .RELEASENOTES
-Created [05/05/2019_08:59]
-Updated [13/05/2019_04:40]
-Updated [22/05/2019_20:13]
-Updated [24/05/2019_19:25]
-Updated [06/06/2019_19:25]
-Updated [09/06/2019_09:18]
-Updated [15/06/2019_01:11]
-Updated [15/06/2019_13:59] Updated Reports
-Updated [01/07/2020_14:43] Script Fle Info was updated
-Updated [01/07/2020_15:42] Script Fle Info was updated
-Updated [01/07/2020_16:07] Script Fle Info was updated
-Updated [01/07/2020_16:13] Script Fle Info was updated
-Updated [06/03/2021_20:58] Script Fle Info was updated
-Updated [15/03/2021_23:28] Script Fle Info was updated
 
-#> 
+
+#>
 
 <#
 .SYNOPSIS
@@ -110,7 +97,7 @@ Function Get-CitrixServerPerformance {
 	}
 	$ServerPerfMon
 
-	if ($Export -eq 'Excel') { 
+	if ($Export -eq 'Excel') {
 		$ExcelOptions = @{
 			Path             = $(Join-Path -Path $ReportPath -ChildPath "\Citrix_Server_Performance-$(Get-Date -Format yyyy.MM.dd-HH.mm).xlsx")
 			AutoSize         = $True
@@ -124,7 +111,7 @@ Function Get-CitrixServerPerformance {
 		}
 		$ServerPerfMon | Export-Excel -Title CitrixServerPerformance -WorksheetName CitrixServerPerformance @ExcelOptions
 	}
-	if ($Export -eq 'HTML') { 
+	if ($Export -eq 'HTML') {
 		$ReportTitle = 'Citrix Server Performance'
         $HeadingText = "$($ReportTitle) [$(Get-Date -Format dd) $(Get-Date -Format MMMM) $(Get-Date -Format yyyy) $(Get-Date -Format HH:mm)]"
         New-HTML -TitleText $($ReportTitle) -FilePath $(Join-Path -Path $ReportPath -ChildPath "\$($ReportTitle.Replace(' ','_'))-$(Get-Date -Format yyyy.MM.dd-HH.mm).html") {
@@ -135,7 +122,7 @@ Function Get-CitrixServerPerformance {
             if ($ServerPerfMon) { New-HTMLTab -Name 'Performance' @TabSettings -HtmlData {New-HTMLSection @TableSectionSettings { New-HTMLTable -DataTable $($ServerPerfMon) @TableSettings}}}
         }
 	}
-	if ($Export -eq 'Host') { 
+	if ($Export -eq 'Host') {
 		$ServerPerfMon
 	}
 } #end Function
